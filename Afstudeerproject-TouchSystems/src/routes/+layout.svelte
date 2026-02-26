332082b0d6b9d58eeece97a84e5d5605b3a75f9a
Berre-O <berre.ongena@student.arteveldehs.be>
2026-02-26 13:49:21 +0100

    Add initial styles and create new route pages for logs, search, and statistics
    - Added _main.css for global styles including body and link styles.
    - Created +page.svelte for logs route.
    - Created +page.svelte for search route with a header.
    - Created +page.svelte for statistics route with a header.

--- Afstudeerproject-TouchSystems/src/routes/+layout.svelte
+++ Afstudeerproject-TouchSystems/src/routes/+layout.svelte
@@ -1,11 +1,92 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import logo from '$lib/assets/touchsystems.png';
+	import '$lib/styles/_main.css';
+	import { page } from '$app/stores';
 
 	let { children } = $props();
+
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
+<header class="header">
+	<img src={logo} alt="TouchSystems Logo" class="logo"/>
+	<h1>TouchSystems</h1>
+</header>
 
-{@render children()}
+<div class="devider">
+	<aside class="aside">
+		<nav class="nav">
+			<ul class="list list--nav">
+				<li class="list__item list__item--nav" class:active={$page.url.pathname === "/"}><a href="/">Dashboard</a></li>
+				<li class="list__item list__item--nav" class:active={$page.url.pathname === "/search"}><a href="/search">Search</a></li>
+				<li class="list__item list__item--nav" class:active={$page.url.pathname === "/statistics"}><a href="/statistics">Statistics</a></li>
+				<li class="list__item list__item--nav" class:active={$page.url.pathname === "/logs"}><a href="/logs">Logs</a></li>
+			</ul>
+		</nav>
+	</aside>
+	
+	<main class="main">
+	{@render children()}
+	</main>
+</div>
+
+<style>
+	.header {
+		display: flex;
+		box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.411);
+		max-height: 8rem;
+		padding: 1rem;
+		z-index: 10;
+		flex-shrink: 0;
+	}
+
+	.aside {
+		width: 8rem;
+		min-height: auto;
+		display: flex;
+		flex-shrink: 0;
+
+		box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+		padding-left: 1rem;
+		padding-top: 1rem;
+
+		z-index: 1;
+	}
+
+	.nav {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.list {
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+	}
+
+	.list__item--nav {
+		padding-left: 1rem;
+		padding-right: 1rem;
+	}
+
+	.active {
+		background-color: pink;
+		border-radius: 0.5rem;
+	}
+
+	.devider {
+		display: flex;
+		flex: 1;
+		height: 100%;
+	}
+
+	.main {
+		flex: 1;
+		overflow: auto;
+	}
+
+	.logo {
+		width: 6rem;
+		height: 6rem;
+		margin-right: 1rem;
+	}
+</style>
