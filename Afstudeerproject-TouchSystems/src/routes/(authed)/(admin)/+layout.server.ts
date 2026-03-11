91c5c25b30adef3ecb199d4a4db2d3b2482b3bb1
Berre-O <berre.ongena@student.arteveldehs.be>
2026-03-11 15:12:53 +0100

    feat: admin only pages

--- Afstudeerproject-TouchSystems/src/routes/(authed)/(admin)/+layout.server.ts
+++ Afstudeerproject-TouchSystems/src/routes/(authed)/(admin)/+layout.server.ts
@@ -1,15 +1,25 @@
-import { error, redirect } from '@sveltejs/kit';
+import { error, redirect } from "@sveltejs/kit";
 
 export const load = async ({ locals }) => {
-    const session = await locals.auth.getSession();
-    
-    if (!session) throw redirect(303, '/login');
+  const { session } = await locals.safeGetSession();
 
-    if (locals.userProfile?.admin !== true) {
-        throw error(403, { message: 'Not Authorized' });
-    }
+  if (!session) throw redirect(303, "/login");
 
-    return {
-        user: locals.userProfile
-    };
+  const { data: profile, error: profileError } = await locals.supabase
+    .from("profile")
+    .select("*")
+    .eq("id", session.user.id)
+    .single();
+
+  if (profileError || !profile) {
+    throw error(500, { message: "Could not fetch profile" });
+  }
+
+  if (profile.admin !== true) {
+    throw error(403, { message: "Not Authorized" });
+  }
+
+  return {
+    user: profile,
+  };
 };
